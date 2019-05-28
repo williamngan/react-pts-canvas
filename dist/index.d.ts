1cbaa87f2a52905bafa22f89f88112a2dba238c1
William Ngan <williamngan@users.noreply.github.com>
2019-05-28 01:26:48 -0700

    fix dependencies again

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {CanvasSpace, Bound, Group, CanvasForm} from "pts";
 
-export interface PtsCanvasProps {
+export interface PtsCanvasProps extends React.Props {
   name?:string,
   background?:string,
   resize?:boolean,
@@ -12,12 +12,13 @@ export interface PtsCanvasProps {
   canvasStyle?:object
 }
 
-declare class PtsCanvas extends React.Component<PtsCanvasProps, any> {
+export default class PtsCanvas extends React.Component<PtsCanvasProps, any> {
   canvRef: React.RefObject;
   space: CanvasSapce;
   form: CanvasForm;
   _touch: boolean;
 
+  constructor( props: Readonly<PtsCanvasProps> );
   animate( time:number, ftime:number ): void;
   start( bound:Bound, space:CanvasSpace ): void;
   resize( size:Group, evt:Event ): void;
